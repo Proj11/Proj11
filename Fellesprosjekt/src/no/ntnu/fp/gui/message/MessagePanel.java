d50bd917ba4e12f3b15b62f6d8a7a85ca2445a2e
Torbjorn Follan <tobbis.follan@gmail.com>
2012-03-19 13:55:01 +0100

    Message GUI

--- Fellesprosjekt/src/no/ntnu/fp/gui/message/MessagePanel.java
+++ Fellesprosjekt/src/no/ntnu/fp/gui/message/MessagePanel.java
@@ -1,18 +1,36 @@
 package no.ntnu.fp.gui.message;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.table.TableModel;
 
+import no.ntnu.fp.gui.employee.EmployeeList;
+import no.ntnu.fp.model.employee.Employee;
 import no.ntnu.fp.model.message.Message;
+import no.ntnu.fp.model.message.MessageTableModel;
 
 public class MessagePanel extends JPanel {
 	
-	public Set<Message> messages;
+	private JScrollPane scrollPane;
+	private JTable elements;
+	private MessageTableModel tableModel;
 	
 	public MessagePanel() {
 		super();
-		messages=new HashSet<Message>();
+		
+		tableModel=new MessageTableModel();
+		tableModel.add(new Message(null));
+		tableModel.add(new Message(null));
+		elements = new JTable(tableModel);
+		elements.getTableHeader().setReorderingAllowed(false);
+		scrollPane = new JScrollPane(elements);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setPreferredSize(new Dimension(280, 450));
+		add(scrollPane);	
 	}
 }
