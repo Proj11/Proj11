5c543ab379c1cf1cb7c16f2a959274bbd3736f25
Torbjorn Follan <tobbis.follan@gmail.com>
2012-03-20 12:28:58 +0100

    MessagePanel GUI ferdig med eksempler

--- Fellesprosjekt/src/no/ntnu/fp/gui/message/MessagePanel.java
+++ Fellesprosjekt/src/no/ntnu/fp/gui/message/MessagePanel.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 import javax.swing.table.TableModel;
 
 import no.ntnu.fp.gui.employee.EmployeeList;
+import no.ntnu.fp.model.appointment.Appointment;
 import no.ntnu.fp.model.employee.Employee;
 import no.ntnu.fp.model.message.Message;
 import no.ntnu.fp.model.message.MessageTableModel;
@@ -18,19 +19,37 @@ public class MessagePanel extends JPanel {
 	private JScrollPane scrollPane;
 	private JTable elements;
 	private MessageTableModel tableModel;
+	private JButton gotoButton;
+	private JPanel tablePanel;
 	
 	public MessagePanel() {
 		super();
-		
+		//making tabemodel for the table
 		tableModel=new MessageTableModel();
-		tableModel.add(new Message(null));
-		tableModel.add(new Message(null));
+		Appointment a=new Appointment(new Employee("Torb"));
+		a.setSubject("Testing");
+		tableModel.add(new Message(a));
+		Appointment b=new Appointment(new Employee("Mats"));
+		b.setSubject("Testing");
+		tableModel.add(new Message(b));
+		
+		//making table to put in scrollpane
 		elements = new JTable(tableModel);
 		elements.getTableHeader().setReorderingAllowed(false);
+		elements.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		//adding scrollpane
 		scrollPane = new JScrollPane(elements);
 		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setPreferredSize(new Dimension(280, 450));
-		add(scrollPane);	
+		scrollPane.setPreferredSize(new Dimension(280, 550));
+		add(scrollPane);
+		
+		//adding gotoButton
+		gotoButton = new JButton("Go to");
+		add(gotoButton);
+		
+		
+		
 	}
 }
