8f7513166401c480b30c67e08a312eac4b55130c
sigurlu <sigurd@sigurd-laptop.(none)>
2012-03-20 15:09:17 +0100

    Laget en tcp server og client, samt login fungerer

---
+++ Fellesprosjekt/src/no/ntnu/fp/server/Constants.java
@@ -0,0 +1,8 @@
+package no.ntnu.fp.server;
+
+public class Constants {
+	final static String FALSE = "0";
+	final static String TRUE = "1";
+	final static String LOGON = "2";
+	final static String LOGOUT = "3";
+}
