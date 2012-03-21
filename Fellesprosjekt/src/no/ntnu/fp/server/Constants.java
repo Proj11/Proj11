caee1cdf0d5cac14e728a5c46df7fab235f47e21
Morten Grannes <lilfvb@gmail.com>
2012-03-21 15:05:18 +0100

    laget createAppointment-metode i CalendarServer, oppdatert Constants

--- Fellesprosjekt/src/no/ntnu/fp/server/Constants.java
+++ Fellesprosjekt/src/no/ntnu/fp/server/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 	final static String FALSE = "0";
 	final static String TRUE = "1";
 	final static String LOGON = "2";
-	final static String LOGOUT = "3";
+	final static String CREATE_APPOINTMENT = "3";
 }
