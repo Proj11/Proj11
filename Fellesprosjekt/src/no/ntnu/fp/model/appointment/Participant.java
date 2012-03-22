980973981099dc4def6b7f2ef39ee537ab3703b1
Morten Grannes <lilfvb@gmail.com>
2012-03-22 14:50:18 +0100

    Endret Participant-constructor, så nå blir ting fucked up.

--- Fellesprosjekt/src/no/ntnu/fp/model/appointment/Participant.java
+++ Fellesprosjekt/src/no/ntnu/fp/model/appointment/Participant.java
@@ -6,16 +6,26 @@ public class Participant {
 
 	private Employee employee;
 	private State state;
+	private int appointmentID;
 
-	public Participant(Employee e, State s) {
+	public Participant(Employee e, State s, int appID) {
 		employee = e;
 		state = s;
+		appointmentID = appID;
 	}
 
 	public State getState() {
 		return state;
 	}
 
+	public int getAppointmentID() {
+		return appointmentID;
+	}
+	
+	public void setAppointmentID(int appointmentID) {
+		this.appointmentID = appointmentID;
+	}
+	
 	public void setState(State state) {
 		this.state = state;
 	}
